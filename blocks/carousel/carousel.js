b2063cc03eaeef3db68af699c0d73160ca6f86c2
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-02-29 10:19:59 -0800

    again

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -40,13 +40,11 @@ function showSlide(block, slideIndex = 0) {
       behavior: 'smooth',
     });
   }
-  if (slideIndex >= slides.length && document.getElementsByClassName('green-three')[0] == style.display = 'block';) {
-    console.log("Green");
-    document.getElementsByClassName('red-one')[0].style.display = 'block';
-    document.getElementsByClassName('green-three')[0].style.display = 'none';
-  }
-  if (slideIndex >= slides.length && document.getElementsByClassName('red-two')[0] == style.display = 'block';) {
-    console.log("Red");
+  if (slideIndex >= slides.length {
+    if ( document.getElementsByClassName('green-three')[0].style-display = 'block') {
+      console.log("Green");
+    }
+   
     document.getElementsByClassName('red-one')[0].style.display = 'block';
     document.getElementsByClassName('green-three')[0].style.display = 'none';
   }
