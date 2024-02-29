f7abbfc07925a487a376309e8ab12e792156871b
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-02-29 14:13:29 -0800

    Update jqscript.js

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".section .button-container").addClass("firstbutton");
+    $(".section .button-container").first().addClass("firstbutton");
 
 
     $(".blue-one .button").click(function(){
