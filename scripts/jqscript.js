4924ff29c96cd676a1cf3b4355d9138042adefe5
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-02-29 14:26:36 -0800

    f

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".button-container").addClass("mainbutton");
-    $(".blue-two .mainbutton")[1].removeClass();
+    $(".blue-two")[1].removeClass("mainbutton");
 
 
     $(".blue-one .button").click(function(){
