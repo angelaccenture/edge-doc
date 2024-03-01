e9583bc3d7e662b30bea04d1d90632230075c648
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-01 09:16:13 -0800

    Update textarea.js

--- blocks/textarea/textarea.js
+++ blocks/textarea/textarea.js
@@ -1,7 +1,1 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
-  const textarea = document.createElement('textarea');
- 
-  block.append(textarea);
-}
