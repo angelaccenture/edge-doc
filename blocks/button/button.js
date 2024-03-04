cf59203243e0ad27317fff88c21f82a0f35fa3b2
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 12:39:52 -0800

    test

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,16 +1,15 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log("test 5");
+  console.log("test 6");
   const main = document.createElement('div');
   [...block.children].forEach((row) => {
     const button = document.createElement('button');
     button.setAttribute('type','button');
     button.className="first";
-    //row.firstElementChild.innerHTML;
     console.log(row.firstElementChild);
 
-    while (row.firstElementChild) button.append(row.firstElementChild.innerHTML);
+    while (row.firstElementChild) button.append(row.firstElementChild);
     [...button.children].forEach((div) => {
       //     div.remove();
       //After I pull class name, then remove these
