39f868e0ab9eaa4ccb0f111aedfc3b3a5e07a669
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 11:27:40 -0800

    going back

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
-    console.log("new test");
-    $(".button-wrapper").next().next().addClass("test");
-});
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const main = document.createElement('div');
+  [...block.children].forEach((row) => {
+    const button = document.createElement('button');
+    console.log(row.firstElementChild);
+    if (row.firstElementChild) button.className="first";
+    else button.className="second";
+    console.log("test new 4");
+
+    while (row.firstElementChild) button.append(row.firstElementChild);
+    [...button.children].forEach((div) => {
+      if (row.firstElementChild) div.className="first";
+      else div.className="second";
+      //if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      //else div.className = 'cards-card-body';
+    });
+    const elements = document.getElementsByClassName("second");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+    main.append(button);
+  });
+  //main.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(main);
+}
 
 
 /*<button type="button" class="VAR" title="VAR">VAR</button>*/
