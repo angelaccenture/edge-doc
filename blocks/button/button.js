477b78d50d659304f80738faf15357c786220d53
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 09:55:09 -0800

    button

---
+++ blocks/button/button.js
@@ -0,0 +1,16 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const button = document.createElement('button');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  block.textContent = '';
+  block.append(ul);
+}
