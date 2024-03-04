a7293889bde8285d31c6691bf94468e9459f193b
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 10:02:47 -0800

    Update button.js

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,19 +1,15 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-const button = document.createElement('button type="button"');
+const button = document.createElement('button');
+	
 
-
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+  const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
+    button.append(li);
   block.append(button);
 }
