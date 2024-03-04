272a89bb91d9f63a7391ed0e458f0f6b24235b5f
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 12:59:23 -0800

    trying this

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,12 +1,13 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log("test 11");
+  console.log("test 2");
   const main = document.createElement('div');
   [...block.children].forEach((row) => {
     const button = document.createElement('button');
     button.setAttribute('type','button');
     button.className=row.firstElementChild.innerHTML;
+    row.firstElementChild.remove();
    
     while (row.firstElementChild) button.append(row.firstElementChild);
     [...button.children].forEach((div) => {
