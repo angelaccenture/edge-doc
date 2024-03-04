72985a34a98893011aa09e9ba64a83e01ec2a79f
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 12:37:39 -0800

    try again

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,14 +1,14 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log("test 3");
+  console.log("test 4");
   const main = document.createElement('div');
   [...block.children].forEach((row) => {
     const button = document.createElement('button');
     button.setAttribute('type','button');
     button.className="first";
     //row.firstElementChild.innerHTML;
-console.log(row.firstElementChild);
+    console.log(row.firstElementChild.innterHTML);
 
   /*  while (row.firstElementChild) button.append(row.firstElementChild);
     [...button.children].forEach((div) => {
