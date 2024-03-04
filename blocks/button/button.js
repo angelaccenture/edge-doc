a3eb47f59fe6b64df2e5b27db651424aafc47332
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-04 11:37:14 -0800

    test

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,7 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log("test 1");
+  console.log("test 2");
   const main = document.createElement('div');
   [...block.children].forEach((row) => {
     const button = document.createElement('button');
@@ -9,7 +9,10 @@ export default function decorate(block) {
     
     while (row.firstElementChild) button.append(row.firstElementChild);
     [...button.children].forEach((div) => {
-      //Need to move class to above and then remove div elements
+      console.log(div.firstChild);
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+
     });
     main.append(button);
   });
