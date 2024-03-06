8848d9141e5b0ed4083038537ebeacf8efc64143
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 15:10:34 -0800

    update

--- blocks/recordexample/recordexample.js
+++ blocks/recordexample/recordexample.js
@@ -1,13 +1,1 @@
-export default function decorate(block) {
-    const main = document.createElement('div');
-    [...block.children].forEach((row) => {
-      const innerdiv = document.createElement('div');
-      innerdiv.className="class";
-       while (row.firstElementChild) div.append(row.firstElementChild);
-      [...div.children].forEach((div) => {
-        div.className="newclass";
-     });
-      main.append(div);
-    });
-    block.append(main);
-  }
+
