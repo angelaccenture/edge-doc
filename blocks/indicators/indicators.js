5efbc09b4ad151b77121569a247e2f5fa048cb11
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 13:37:00 -0800

    try

--- blocks/indicators/indicators.js
+++ blocks/indicators/indicators.js
@@ -3,8 +3,10 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ol = document.createElement('ol');
+  ol.className = 'carousel-slide-indicators';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.className="carousel-slide-indicator";
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
