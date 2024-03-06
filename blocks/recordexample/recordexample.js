294177a407bcccf61d7c9c6ec29dbc114881bc7e
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 15:12:42 -0800

    Update recordexample.js

--- blocks/recordexample/recordexample.js
+++ blocks/recordexample/recordexample.js
@@ -1,8 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
+  const main = document.createElement('div');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
@@ -10,7 +9,7 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
+    main.append(li);
   });
-  block.append(ul);
+  block.append(main);
 }
