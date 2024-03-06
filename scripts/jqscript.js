47d648a487abe9b6637d8efef48e29433d09ba3c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 11:46:44 -0800

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -71,22 +71,13 @@ $(document).ready(function() {
       $('#form-believed').val($(this).html());    
     });
     $(".formbutton").on("click", ".secondformv", function() {
-      console.log("secondformv");
-      $('#form-described').val("");
-      $('#form-described').val($(this).html());    
+      $('#form-describe').val("");
+      $('#form-describe').val($(this).html());    
     }); 
     $(".formbutton").on("click", ".thirdformv", function() {
       $('#form-makefeel').val("");
       $('#form-makefeel').val($(this).html());    
     });
-    /*Show next buttons when user enters in form */
-    $("#form-believed").change(function(){
-     console.log("YES");
- });
-    $("#form-believed").on("input propertychange", function() {
-      console.log("value yes");
-      console.log($(this).val()); 
-   });
 /*Post
       $(".red-four").click(function(){
         $(".red-five").show();
