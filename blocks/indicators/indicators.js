aae637aa4cf9cc36a807b19e9a1239de1e1bee42
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 14:43:27 -0800

    Update indicators.js

--- blocks/indicators/indicators.js
+++ blocks/indicators/indicators.js
@@ -3,7 +3,7 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ol = document.createElement('ol');
-  const parent = document.querySelector("div").classList.contains(".indicators");
+  const parent = ol.parentElement;
   console.log(parent);
   //parent.className("carousel");
   ol.className = 'carousel-slide-indicators';
