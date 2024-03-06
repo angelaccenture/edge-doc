a852f31f2a420d017ddf45a443607d5f50c404ff
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-05 18:27:41 -0800

    Updates

---
+++ blocks/formbutton/formbutton.js
@@ -0,0 +1,21 @@
+
+export default function decorate(block) {
+  const main = document.createElement('div');
+  [...block.children].forEach((row) => {
+    const button = document.createElement('button');
+    button.setAttribute('type','button');
+    button.className=row.firstElementChild.innerHTML;
+    row.firstElementChild.remove();
+   
+    while (row.firstElementChild) button.append(row.firstElementChild);
+    [...button.children].forEach((div) => {
+      //replace div with span tag
+   });
+    main.append(button);
+  });
+  block.textContent = '';
+  block.append(main);
+}
+
+
+/*<button type="button" class="VAR">VAR</button>*/
