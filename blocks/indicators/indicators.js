dab066be8aa94d4d11b230b1c686d2f098673daa
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 13:31:40 -0800

    adding indicators

---
+++ blocks/indicators/indicators.js
@@ -0,0 +1,18 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ol = document.createElement('ol');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ol.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ol);
+}
