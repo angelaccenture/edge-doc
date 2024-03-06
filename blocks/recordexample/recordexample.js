49faf9b9ddad700473ab0af171203431cf82002b
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-06 15:14:12 -0800

    lets see

--- blocks/recordexample/recordexample.js
+++ blocks/recordexample/recordexample.js
@@ -1,15 +1,13 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const main = document.createElement('div');
-  [...block.children].forEach((row) => {
+   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    main.append(li);
   });
-  block.append(main);
+  block.append(li);
 }
