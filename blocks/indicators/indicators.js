6e25c6393fc128df261502fda62e7bf6280aa8d1
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-07 10:37:42 -0800

    Update indicators.js

--- blocks/indicators/indicators.js
+++ blocks/indicators/indicators.js
@@ -2,7 +2,7 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
  const divclass = document.getElementsByClassName("indicators");
- divclass.classList("carousel");
+ divclass.className("carousel");
  console.log(divclass);
  
   const ol = document.createElement('ol');
