bd37450c69dea5f451df90dddf40ac7c69856173
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-08 08:56:41 -0800

    try

--- blocks/indicators/indicators.js
+++ blocks/indicators/indicators.js
@@ -11,13 +11,13 @@ export default function decorate(block) {
     const li = document.createElement('li');
     li.className="carousel-slide-indicator";
     const firstli = document.getElementsByClassName('carousel-slide-indicator');
-    console.log(firstli[0]);
     const button = document.createElement('button');
     button.setAttribute('type','button');
     li.append(button);
      while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-         //removeChild(div);
+      console.log(firstli[0]);   
+      //removeChild(div);
     });
     ol.append(li);
   });
