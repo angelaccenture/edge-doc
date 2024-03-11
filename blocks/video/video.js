a885843a8da050181225a66d55709e4f12f4adb2
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2024-03-11 16:02:01 -0500

    add iframe

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -6,18 +6,13 @@ export default function decorate(block) {
     const videoPlayer = document.createElement('div');
     videoPlayer.className = 'video-player';
 
-    const videoElement = document.createElement('video');
-    videoElement.setAttribute('controls', '');
-    videoElement.setAttribute('autoplay', '');
-    videoElement.setAttribute('muted', '');
-    videoElement.setAttribute('aria-label', 'Video Player');
-
-    const sourceElement = document.createElement('source');
-    sourceElement.setAttribute('src', videoData.url);
-    sourceElement.setAttribute('type', 'video/mp4');
-
-    videoElement.appendChild(sourceElement);
-    videoPlayer.appendChild(videoElement);
+    const iframeElement = document.createElement('iframe');
+    iframeElement.setAttribute('src', videoData.url);
+    iframeElement.setAttribute('frameborder', '0');
+    iframeElement.setAttribute('allowfullscreen', '');
+    iframeElement.setAttribute('width', '560');
+    iframeElement.setAttribute('height', '315');
+    videoPlayer.appendChild(iframeElement);
 
     block.textContent = '';
     block.appendChild(videoPlayer);
@@ -34,10 +29,10 @@ if (!videoWrapper) {
 // Create the video block
 const videoBlock = document.createElement('div');
 videoBlock.id = 'video-block';
-videoBlock.setAttribute('data-url', 'https://myoffice.accenture.com/personal/j_c_thomas_accenture_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fj%5Fc%5Fthomas%5Faccenture%5Fcom%2FDocuments%2FMicrosoft%20Teams%20Chat%20Files%2Fseed%2D225721745%5Fscene%2D0%5Fprofile%2Dcolorful%5Fstrength%2D100%2Emp4&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview');
+videoBlock.setAttribute('data-url', 'https://www.youtube.com/embed/NPJNPrshhTo?si=nk67foSYY02f4ISg&amp'); // Example YouTube embed URL
 
 // Append the video block to the video wrapper
 videoWrapper.appendChild(videoBlock);
 
 // Call the decorate function on the newly created video block
-// decorate(videoBlock);
+decorate(videoBlock);
