f0e69c432385d915e3b8ae6e13d87da79339401e
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2024-03-11 16:13:08 -0500

    Update video.js

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -10,15 +10,12 @@ export default function decorate(block) {
     iframeElement.setAttribute('src', videoData.url);
     iframeElement.setAttribute('frameborder', '0');
     iframeElement.setAttribute('allowfullscreen', '');
-    iframeElement.setAttribute('width', '560');
-    iframeElement.setAttribute('height', '315');
     videoPlayer.appendChild(iframeElement);
 
     block.textContent = '';
     block.appendChild(videoPlayer);
 }
 
-// Create or find the video wrapper
 let videoWrapper = document.querySelector('.video-wrapper');
 if (!videoWrapper) {
     videoWrapper = document.createElement('div');
@@ -26,14 +23,12 @@ if (!videoWrapper) {
     document.body.appendChild(videoWrapper);
 }
 
-// Create the video block
 const videoBlock = document.createElement('div');
 videoBlock.id = 'video-block';
-videoBlock.setAttribute('data-url', 'https://www.youtube.com/embed/NPJNPrshhTo?si=nk67foSYY02f4ISg&amp'); // Example YouTube embed URL
+videoBlock.setAttribute('data-url', 'https://www.youtube.com/embed/NPJNPrshhTo?si=nk67foSYY02f4ISg&amp');
 
-// Ensure only one video block is appended to the video wrapper
 videoWrapper.innerHTML = '';
 videoWrapper.appendChild(videoBlock);
 
-// Call the decorate function on the newly created video block
 decorate(videoBlock);
+
