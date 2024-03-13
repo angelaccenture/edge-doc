58548383d101f091fe51e7447e7b994d016e2ba4
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 14:02:25 -0700

    updates

--- blocks/progress/progress.js
+++ blocks/progress/progress.js
@@ -1,19 +1,4 @@
 var elements = document.getElementsByClassName("blueendone");
-console.log(elements);
-
-/*var timeleft = 10;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-  }
-  document.getElementById("progressBar").value = 10 - timeleft;
-  timeleft -= 1;
-}, 1000);
-*/
-
-
-
-var elements = document.getElementsByClassName("blueendone");
 var myFunction = function() {
   console.log("start progress bar");
     var timeleft = 100;
