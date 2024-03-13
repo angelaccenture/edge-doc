ff40f09b253502bf51445786fd12f5eac55df590
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 14:43:59 -0700

    updates

--- blocks/progress/progress.js
+++ blocks/progress/progress.js
@@ -1,19 +1,29 @@
-/*var elements = document.getElementsByClassName("blueendone");
+var elements = document.getElementsByClassName("blueendone");
 var myFunction = function() {
   console.log("start progress bar");
-    var timeleft = 10;
-    var downloadTimer = setInterval(function(){
-      if(timeleft <= 0){
-        clearInterval(downloadTimer);
+    var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
       }
-      document.getElementById("progressBar").value = 10 - timeleft;
-      timeleft -= 1;
-    }, 1000);
+    }
+  }
+}
 };
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', myFunction, false);
-}*/
+}
 
 export default function decorate(block) {
   const ul = document.createElement('div');
