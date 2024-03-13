16694f5ca9c137dec59503bc0634ede63bc6625a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 08:07:16 -0700

    updates

--- blocks/countdown/countdown.js
+++ blocks/countdown/countdown.js
@@ -11,6 +11,21 @@ var downloadTimer = setInterval(function(){
 
 
 export default function decorate(block) {
-    const count = document.createElement('<div id="countdown"></div>');
-    block.append(count);
+    const main = document.createElement('div');
+    [...block.children].forEach((row) => {
+      const button = document.createElement('button');
+      button.setAttribute('type','button');
+      button.className=row.firstElementChild.innerHTML;
+      row.firstElementChild.remove();
+     
+      while (row.firstElementChild) button.append(row.firstElementChild);
+      [...button.children].forEach((div) => {
+        const removediv = div.parentNode;
+        removediv.insertBefore(div.firstChild, div);
+        removediv.removeChild(div);
+     });
+      main.append(button);
+    });
+    block.textContent = '';
+    block.append(main);
   }
