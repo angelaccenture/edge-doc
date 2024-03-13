4a3283a3b8b11e9c7c3c6135cb56177550ddf403
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 14:38:46 -0700

    updates

--- blocks/progress/progress.js
+++ blocks/progress/progress.js
@@ -8,7 +8,7 @@ var myFunction = function() {
       }
       document.getElementById("progressBar").value = 10 - timeleft;
       timeleft -= 1;
-    }, 100000);
+    }, 1000);
 };
 
 for (var i = 0; i < elements.length; i++) {
