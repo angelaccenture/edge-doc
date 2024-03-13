f782d4a901e4bbc0ca7ac39b63b4a29e6eb8b3c9
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 08:05:35 -0700

    updates

--- blocks/countdown/countdown.js
+++ blocks/countdown/countdown.js
@@ -1,1 +1,16 @@
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
 
+
+export default function decorate(block) {
+    const count = document.createElement('<div id="countdown"></div>');
+    block.append(count);
+  }
