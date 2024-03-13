97d93163f1259bde5c081fed6a5fd00a3f647b66
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 13:44:18 -0700

    update

--- blocks/progress/progress.js
+++ blocks/progress/progress.js
@@ -1,6 +1,7 @@
 var elements = document.getElementsByClassName("blueendone");
 
 var myFunction = function() {
+  console.log("start progress bar");
     var timeleft = 100;
     var downloadTimer = setInterval(function(){
       if(timeleft <= 0){
