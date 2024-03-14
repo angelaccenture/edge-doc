d8a8b51770eec3ba78a9c230ebee2b3677d688ea
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-14 08:52:11 -0700

    see

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -18,8 +18,7 @@ $(document).ready(function() {
     $(".blue-three").hide();
   });
   $('.button').on("click", ".greenonebutton", function() {
-   // $('.green-one').animate({width: 0 });
-    $(".green-one").hide("slide", { direction: "left" }, 1200);
+    $(".green-one").hide("slide", { direction: "left" }, 200);
     $(".green-two").delay(400).show("slide", { direction: "right" }, 1200);
   });
   $('.button').on("click", ".greentwobutton", function() {
