7c595eeceb73afb3c78bd15e3d979e513157ee11
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-14 11:15:02 -0700

    trythis

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -1,4 +1,4 @@
-$(document).on("ready", function() {
+$(document).on("load", function() {
   /*onload animation*/
  // $(".blue-one").delay(400).show("slide", { direction: "left" }, 1200);
  // $(".blue-one h1").delay(400).show("slide", { direction: "down" }, 1200);
