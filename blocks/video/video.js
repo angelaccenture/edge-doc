12babc4ccd3d8c84803506fb35d47bd3cd57a4ba
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-13 17:23:20 -0700

    add video player

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,1 +1,23 @@
-
+export default function decorate(block) {
+    const video = document.createElement('video');
+    video.setAttribute("width","320");
+    video.setAttribute("height","240");
+    video.setAttribute("controls");
+    [...block.children].forEach((row) => {
+      const li = document.createElement('source');
+      li.setAttribute("src","movie.mp4");
+      li.setAttribute("type","video/mp4");
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        removediv.removeChild(div);
+      });
+      video.append(li);
+    });
+    block.textContent = '';
+    block.append(video);
+  }
+  /*<video width="320" height="240" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>*/
