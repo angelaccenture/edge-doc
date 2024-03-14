801b067de04e8634a62b3f2085a504d4f3ba1ea7
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-14 11:18:07 -0700

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -1,6 +1,6 @@
 $(document).ready (function() {
   /*onload animation*/
- // $(".blue-one").delay(400).show("slide", { direction: "left" }, 1200);
+  $(".blue-one").delay(400).show("slide", { direction: "left" }, 1200);
  // $(".blue-one h1").delay(400).show("slide", { direction: "down" }, 1200);
   initAudio();
 
