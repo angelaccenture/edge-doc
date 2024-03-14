ef6e02495057d37e784b7cd56ba5bbf9474e50b1
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-14 07:33:41 -0700

    updates

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -4,10 +4,13 @@ export default function decorate(block) {
     video.setAttribute("width","640");
     video.setAttribute("height","480");
     video.setAttribute("controls","");
+    video.setAttribute("autoplay","");
     [...block.children].forEach((row) => {
       const li = document.createElement('source');
-      li.setAttribute("src","/blocks/video/seed-225721745_scene-0_profile-colorful_strength-100.mp4");
-      li.setAttribute("type","video/mp4");
+    //  li.setAttribute("src","/blocks/video/seed-225721745_scene-0_profile-colorful_strength-100.mp4");
+     // li.setAttribute("type","video/mp4");
+       li.setAttribute("src","https://10.89.130.229/test/stream_0/stream.m3u8");
+      li.setAttribute("type","video/x-mpegURL");
       while (row.firstElementChild) li.append(row.firstElementChild);
       [...li.children].forEach((div) => {
         //removediv.removeChild(div);
@@ -20,7 +23,14 @@ export default function decorate(block) {
 
     var video = document.getElementById("video-player");
     console.log(video);
+/*New HTML*
 
+<video controls autoplay>
+<source src="
+https://10.89.130.229/test/stream_0/stream.m3u8"
+type="application/x-mpegURL">
+</video>
+*/
   //const video = document.getElementById("video-player"); 
 
   /*  
