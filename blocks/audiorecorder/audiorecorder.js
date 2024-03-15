c56da079035074af0fa9796064c555862eb0c03a
Josh Boyle <joshua.boyle@accenture.com>
2024-03-15 16:17:15 -0400

    Further along

--- blocks/audiorecorder/audiorecorder.js
+++ blocks/audiorecorder/audiorecorder.js
@@ -3,5 +3,6 @@
  * Record audio from the end user
  */
 export default async function decorate(block) {
-  block.innerHtml('<input type="file" accept="audio/*" capture id="recorder" />');
+  const input = document.createElement('input');
+  block.appendChild(input);
 }
