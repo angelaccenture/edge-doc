5085cb3277bade08be1526e7a75c9bb1cbcc6dc5
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-15 15:24:19 -0700

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -184,7 +184,7 @@ $('.button').on("click", ".blueendone", function() {
   $(".blue-end-one").fadeOut();     
   $(".blue-end-two").delay(400).fadeIn();
  /*Animation on words - green two*/
-    $( ".blue-end-two h4" ).each(function(item, index) {
+    $( ".blue-end-two h4" ).each(function(index) {
      $(this).hide().delay(7000 * index).show(300);
      /*$(item).delay(7000*index).show(300, function() {
             $(this).delay(7000).hide(300);
