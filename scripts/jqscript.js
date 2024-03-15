16d2101f13fe88f49a311e1bfab3d6148570cea3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-15 13:45:04 -0700

    try this

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -183,14 +183,12 @@ $('.button').on("click", ".blueendone", function() {
   playAudioRandom(TapSounds); 
   $(".blue-end-one").fadeOut();     
   $(".blue-end-two").delay(400).fadeIn();
-
-
-}
  /*Animation on words - green two*/
- $( ".blue-end-two h4" ).each(function( index ) {
-  // $(this).delay(400).fadeIn();
-  $( this ).text();
-   console.log( index + ": " + $( this ).text() );
+    $( ".blue-end-two h4" ).each(function( index ) {
+      // $(this).delay(400).fadeIn();
+      $( this ).text();
+      console.log( index + ": " + $( this ).text() );
+    });
 });
 $('.button').on("click", ".blueendtwo", function() {
   playAudioRandom(TapSounds);
