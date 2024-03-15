2daa5665cecd754b7e76e592c43d597bf37e47e8
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-15 15:51:31 -0700

    tryagain

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -188,8 +188,8 @@ $('.button').on("click", ".blueendone", function() {
     $( ".blue-end-two h4" ).each(function(index) {   
      //$(this).hide(); 
      //$(this).delay(7000 * index).show(300);
-     $(this).delay(7000*index).show(300, function() {
-            $(this).delay(7000).fadeOut(300);
+     $(this).delay(7000*index).fadeIn (300, function() {
+            $(this).delay(7000).fadeOut();
       });
     });
   }
