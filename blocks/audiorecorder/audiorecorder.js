d63490886b15450ba8cf5e92aa8a9c777a5f5f52
Josh Boyle <joshua.boyle@accenture.com>
2024-03-15 16:49:19 -0400

    Further along

--- blocks/audiorecorder/audiorecorder.js
+++ blocks/audiorecorder/audiorecorder.js
@@ -1,4 +1,34 @@
-/*
+function setup() {
+  const player = document.getElementById('player');
+
+  const handleSuccess = function (stream) {
+    if (window.URL) {
+      player.srcObject = stream;
+    } else {
+      player.src = stream;
+    }
+  };
+
+  const handleAudioSuccess = async function(stream) {
+    const context = new AudioContext();
+    const source = context.createMediaStreamSource(stream);
+
+    await context.audioWorklet.addModule("processor.js");
+    const worklet = new AudioWorkletNode(context, "worklet-processor");
+
+    source.connect(worklet);
+    worklet.connect(context.destination);
+  };
+
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(handleSuccess);
+
+  navigator.mediaDevices
+    .getUserMedia({audio: true, video: false})
+    .then(handleAudioSuccess);
+}
+
+/**
  * Audio Recorder Block
  * Record audio from the end user
  */
@@ -15,4 +45,6 @@ export default async function decorate(block) {
 
   block.appendChild(audio);
   block.appendChild(input);
+
+  setup();
 }
