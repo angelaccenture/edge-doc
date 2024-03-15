a26fb455e505ca6bd0d9756de5283c62c218ecab
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-15 15:31:51 -0700

    testthis

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -184,7 +184,9 @@ $('.button').on("click", ".blueendone", function() {
   $(".blue-end-one").fadeOut();     
   $(".blue-end-two").delay(400).fadeIn();
  /*Animation on words - green two*/
+ var i = 0;
  function repeatAnim() {
+  i++;
     $( ".blue-end-two h4" ).each(function(index) {
       console.log("Test Item" + index);
      $(this).hide().delay(7000 * index).show(300);
