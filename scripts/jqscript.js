1d4e16fc7f4ace58c3d92ab3ebf1463d73ef7f49
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 10:46:28 -0700

    trythis

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -41,8 +41,8 @@ $(document).ready (function() {
     /*Animation on words - green two*/
 function repeatAnimGreen() {
   $( ".green-two h4" ).each(function(index) { 
-   $(this).delay(7000*index).slideDown (5000, function() {
-        $(this).delay(300).fadeOut();
+   $(this).delay(7000*index).slideDown (2000, function() {
+        $(this).delay(5000).fadeOut();
     });
    /* $(this).delay(7000*index).fadeIn (4000, function() {
         $(this).delay(3000).fadeOut();
