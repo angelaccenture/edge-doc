d385d7f5b4f391cd27bd27e6d353f41c6b7d39ca
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-15 17:27:45 -0700

    tryagain

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -42,12 +42,9 @@ $(document).ready (function() {
 function repeatAnimGreen() {
   $( ".green-two strong" ).each(function(index) {   
     $(this).delay(7000*index).fadeIn (4000, function() {
-      $(this).delay(4000).fadeOut();
+      $(this).delay(3000).fadeOut();
     });
-    console.log(index);
     if (index == 4) {
-      console.log("if 4");
-      console.log(($this));
       $(this).show();
     }
   });
