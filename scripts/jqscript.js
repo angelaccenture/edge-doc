c4ae35eeb666e4281ad494ccea2c66b049d04b1a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 09:15:44 -0700

    tryagain

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -74,7 +74,7 @@ function repeatAnimGreen() {
       $(".form .first, .formnextfirst").hide("slide", { direction: "left" }, 600);
       $(".firstformv").hide("slide", { direction: "left" }, 600);
 
-      $(".form .second").delay(600).show("slide", { direction: "right" }, 600).addClass("showgrid");
+      $(".form .second").show("slide", { direction: "right" }, 1000).addClass("showgrid");
       //$(".secondformv").show("slide", { direction: "right" }, 1200);
       //$(".red-three .default-content-wrapper").show("slide", { direction: "right" }, 1200);
 
