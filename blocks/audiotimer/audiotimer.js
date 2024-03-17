dd5795ec8893613440703d801c77cef94f32b7d2
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 17:40:59 -0700

    updates

--- blocks/audiotimer/audiotimer.js
+++ blocks/audiotimer/audiotimer.js
@@ -1,16 +1,25 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default function decorate(block) {
-  const audiotimer = document.createElement('label');
-  audiotimer.setAttribute("id","video-player");
-   [...block.children].forEach((row) => {
-     while (row.firstElementChild) li.append(row.firstElementChild);
-     [...li.children].forEach((div) => {
-       //removediv.removeChild(div);
-     });
-     audiotimer.append(li);
-   });
-   block.textContent = '';
-   block.append(audiotimer);
- }
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
+}
+
+
+
+
 /*Audio Timer*/
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
