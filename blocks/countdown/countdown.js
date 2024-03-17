0dcb54d7a7ef49eef2785eeaba05762a84d37dd6
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 17:53:47 -0700

    trythis

--- blocks/countdown/countdown.js
+++ blocks/countdown/countdown.js
@@ -8,6 +8,23 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(main);
 }
+export default async function decorate(block) {
+  const audioTimerm = document.createElement('label');
+  audioTimerm.setAttribute('id', 'minutes');
+  audioTimerm.innerText = '00';
+
+  //const audioTimerd = document.createElement(':');
+
+  const audioTimers = document.createElement('label');
+  audioTimers.setAttribute('id', 'seconds');
+  audioTimers.innerText = '00';
+
+  block.appendChild(audioTimerm);
+ // block.appendChild(audioTimerd);
+  block.appendChild(audioTimers);
+
+  audioTimer();
+}
 
 
 
@@ -27,13 +44,37 @@ var myFunction = function() {
   }
   timeleft -= 1;
 }, 1000);
-  console.log("Start Recording");
-  audioTimer();
-  recorder.hide();
 };
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', myFunction, false);
 }
+
+
+function audioTimer() {
+  /*Audio Timer*/
+  var minutesLabel = document.getElementById("minutes");
+  var secondsLabel = document.getElementById("seconds");
+  var totalSeconds = 0;
+  setInterval(setTime, 1000);
+  
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+  
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+  
+  }
+  
+  
 
 
