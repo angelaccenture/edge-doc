2454729ab3833ae9f1999bd01380b914141da720
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 17:42:41 -0700

    updates

--- blocks/audiotimer/audiotimer.js
+++ blocks/audiotimer/audiotimer.js
@@ -1,20 +1,9 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+export default async function decorate(block) {
+  const audioTimer = document.createElement('label');
+  startBtn.setAttribute('id', 'startRecording');
+  startBtn.innerText = 'Start Recording';
 
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
+  block.appendChild(startBtn);
 }
 
 
