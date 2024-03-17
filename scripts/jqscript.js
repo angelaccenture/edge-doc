87d011b710ecde08ce326c0318f633cf44e848af
Josh Boyle <joshua.boyle@accenture.com>
2024-03-16 20:42:13 -0400

    Fixed

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -130,7 +130,7 @@ function repeatAnimGreen() {
 
       fetch(`https://adobeioruntime.net/api/v1/web/18501-631graycheetah/default/fourthPromptAction?firstResponse=${responses.firstResponse}&secondResponse=${responses.secondResponse}&thirdResponse=${responses.thirdResponse}`)
         .then((response) => {
-          const reader = response.body.getReader();
+          let reader = response.body.getReader();
           reader.on('data', chunk => {
             console.log(chunk.toString());
           });
