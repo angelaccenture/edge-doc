f395c91696371b202b1f39dc6c9aae0aa848521a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-16 17:58:42 -0700

    trythis

--- blocks/countdown/countdown.js
+++ blocks/countdown/countdown.js
@@ -7,22 +7,6 @@ export default function decorate(block) {
   });
   block.textContent = '';
   block.append(main);
-
-  const audioTimerm = document.createElement('label');
-  audioTimerm.setAttribute('id', 'minutes');
-  audioTimerm.innerText = '00';
-
-  //const audioTimerd = document.createElement(':');
-
-  const audioTimers = document.createElement('label');
-  audioTimers.setAttribute('id', 'seconds');
-  audioTimers.innerText = '00';
-
-  block.appendChild(audioTimerm);
-  // block.appendChild(audioTimerd);
-   block.appendChild(audioTimers);
- 
-  // audioTimer();
 }
 
 var elements = document.getElementsByClassName("recordstory");
