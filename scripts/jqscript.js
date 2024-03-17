09ce0e8865ee013248fa73585ef5e6b165fcf607
Josh Boyle <joshua.boyle@accenture.com>
2024-03-16 20:14:07 -0400

    Fixed

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -128,8 +128,8 @@ function repeatAnimGreen() {
           'thirdResponse': 'calm'
         };
 
-        fetch(`https://adobeioruntime.net/api/v1/web/18501-631graycheetah/default/fourthPromptAction?firstResponse=${responses.firstResponse}&secondResponse=${responses.secondResponse}&thirdResponse=${responses.thirdResponse}`).then(success => {
-          console.log('JOSH HERE IS THE RESPONSE: ' + success.json().prompt);
+        fetch(`https://adobeioruntime.net/api/v1/web/18501-631graycheetah/default/fourthPromptAction?firstResponse=${responses.firstResponse}&secondResponse=${responses.secondResponse}&thirdResponse=${responses.thirdResponse}`).then((success) => {
+          console.log('JOSH HERE IS THE RESPONSE: ' + success.body.prompt);
         });
       });
 
