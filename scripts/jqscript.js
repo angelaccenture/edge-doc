76ad4fc9fd42687e2c07a4d318152540c9dcbb1c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-17 12:08:34 -0700

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -275,7 +275,7 @@ $('.button').on('click', '.bluethreebutton', function () {
       $('.audiorecorder-wrapper').show();
       setTimeout(function() {
         $('.audiotimer-wrapper').show();
-      }, 3500);
+      }, 3600);
      
 
       $(".button .stoprecording").delay(33000).queue(function(next){
