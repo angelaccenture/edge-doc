bb6515c5c04c8ad523b3deb56e32c951bd7f3147
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-20 15:47:39 -0700

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -53,11 +53,12 @@ $('.button').on('click', '.bluethreebutton', function () {
       $('.green-two h4')
         .each(function (index) {
           $(this).delay(1000 * index).slideDown(500, function () {
-            console.log("test");
+            //  $(this).delay(5000 * index).hide();
               if (index == 4) {
                 //console.log("Last One");
               } else {
-                $(this).delay(6000).hide();
+                //$(this).delay(6000).hide();
+                $(this).delay(5000 * index).hide();
               }
             });
 
