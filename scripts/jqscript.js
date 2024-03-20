478fda41cfb8ea3841221f4efb2546841dd97c33
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-20 15:30:44 -0700

    trythis

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -60,7 +60,7 @@ $('.button').on('click', '.bluethreebutton', function () {
                 //console.log("Last One");
               } else {
                 $(this)
-                  .delay(1000).slideDown();
+                  .delay(1000).hide();
               }
             });
 
