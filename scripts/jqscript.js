337ec0a13ba03ebb958b3db144295dcdeb7fc814
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-20 15:42:06 -0700

    trythis

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -53,7 +53,7 @@ $('.button').on('click', '.bluethreebutton', function () {
       $('.green-two h4')
         .each(function (index) {
           $(this)
-            .delay(4000 * index)
+            .delay(2000 * index)
             .slideDown(500, function () {
               if (index == 4) {
                 //console.log("Last One");
