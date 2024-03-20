8b7d3df8b48b667e65e19ff8981dd3c0abbcdb4a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-20 11:25:47 -0700

    updates

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -332,12 +332,9 @@ $('.button').on('click', '.bluethreebutton', function () {
             $('.blue-end-two h4')
               .each(function (index) {
                 $(this).delay(7000 * index).fadeIn(4000, function () {
-                    $(this).delay(3000, function() {
+                $(this).delay(3000).fadeOut(function() {
                         if(index === 2) {
                             repeatAnim();
-                        }
-                        else {
-                          $(this).fadeOut();
                         }
                       })
                     
