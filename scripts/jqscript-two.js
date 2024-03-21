ab7abbe6839b72245b322726da08d25d421ea878
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-21 10:29:50 -0700

    trythis

--- scripts/jqscript-two.js
+++ scripts/jqscript-two.js
@@ -19,7 +19,7 @@ $(document).ready (function() {
             if (index == 4) {
               //console.log("Last One");
             } else {
-              $(this).delay(2000).hide();
+              $(this).hide();
           }
         });
 
