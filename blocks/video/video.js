2c0a62bc4a92f4cf0f9aa50e9b3afd5e47f82ab8
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-22 08:52:31 -0700

    updates

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -3,11 +3,11 @@ export default function decorate(block) {
     video.setAttribute("id","video");
     video.setAttribute("controls","");
    // video.setAttribute("autoplay","");
-    video.setAttribute("src","https://genheroes.accenture.com/test/stream_0/stream.m3u8");
+   // video.setAttribute("src","https://genheroes.accenture.com/test/stream_0/stream.m3u8");
     [...block.children].forEach((row) => {
       const li = document.createElement('source');
-    //  li.setAttribute("src","https://genheroes.accenture.com/test/stream_0/stream.m3u8");
-     // li.setAttribute("type","video/mp4");
+      li.setAttribute("src","https://genheroes.accenture.com/test/stream_0/stream.m3u8");
+      li.setAttribute("type","video/m3u8");
     //   li.setAttribute("src","https://10.89.130.229/test/stream_0/stream.m3u8");
     //  li.setAttribute("type","video/x-mpegURL");
       while (row.firstElementChild) li.append(row.firstElementChild);
