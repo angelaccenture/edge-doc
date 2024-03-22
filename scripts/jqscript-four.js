63cc8db2c79df1aeb1c82d2030d45160ee29749c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2024-03-22 14:03:41 -0700

    updates

--- scripts/jqscript-four.js
+++ scripts/jqscript-four.js
@@ -33,7 +33,7 @@ if (stopFunc == false) {
        .each(function (index) {
          $(this).delay(7000 * index).fadeIn(4000, function () {
          $(this).delay(3000).fadeOut(function() {
-                 if(index === 13) {
+                 if(index === 15) {
                      repeatAnim();
                  }
                })
